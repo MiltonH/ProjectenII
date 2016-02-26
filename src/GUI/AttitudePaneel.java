1e5fa3177601e14ae95876f2686c19947b49699c
Dries Meert <Dries Meert@Dries>
2016-02-26 18:07:29 +0100

    AttitudePaneel aanpassing uitzicht + toevoeging aandacht knop

--- src/GUI/AttitudePaneel.java
+++ src/GUI/AttitudePaneel.java
@@ -4,16 +4,20 @@ import java.util.Collections;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.HPos;
-import javafx.geometry.Point2D;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import static javafx.scene.layout.Region.USE_PREF_SIZE;
 import javafx.scene.layout.RowConstraints;
+import javafx.stage.Screen;
 
 public class AttitudePaneel extends GridPane {
 
@@ -21,24 +25,28 @@ public class AttitudePaneel extends GridPane {
 
     public AttitudePaneel(HoofdPaneel hoofdPanel) {
         setId("inlogPaneelBG");
+        //schermformaat
+        Rectangle2D schermformaat = Screen.getPrimary().getVisualBounds();
+        
         //aanmaak grid
         gridLinesVisibleProperty().set(false);
 
         ColumnConstraints kolom0 = new ColumnConstraints();
-        kolom0.setPercentWidth(5);
+        kolom0.setPercentWidth(15);
         ColumnConstraints kolom1 = new ColumnConstraints();
-        kolom1.setPercentWidth(90);
+        kolom1.setPercentWidth(70);
         ColumnConstraints kolom2 = new ColumnConstraints();
-        kolom2.setPercentWidth(5);
+        kolom2.setPercentWidth(15);
+        kolom2.setHalignment(HPos.CENTER);
 
         getColumnConstraints().addAll(kolom0, kolom1, kolom2);
 
         RowConstraints rij0 = new RowConstraints();
-        rij0.setPercentHeight(5);
+        rij0.setPercentHeight(15);
         RowConstraints rij1 = new RowConstraints();
-        rij1.setPercentHeight(90);
+        rij1.setPercentHeight(70);
         RowConstraints rij2 = new RowConstraints();
-        rij2.setPercentHeight(5);
+        rij2.setPercentHeight(15);
 
         getRowConstraints().addAll(rij0, rij1, rij2);
 
@@ -86,6 +94,7 @@ public class AttitudePaneel extends GridPane {
 
         ColumnConstraints kolom0listViewPane = new ColumnConstraints();
         kolom0listViewPane.setPercentWidth(100);
+        kolom0listViewPane.setHalignment(HPos.CENTER);
         textAreaPane.getColumnConstraints().addAll(kolom0listViewPane);
 
         RowConstraints rij0listViewPane = new RowConstraints();
@@ -134,19 +143,20 @@ public class AttitudePaneel extends GridPane {
         //Label
         Label attitudeLabel = new Label("Attitude");
         attitudeLabel.setId("inlognaamNummerLabel");
+        attitudeLabel.setId("attitudeLabel");
         textAreaPane.add(attitudeLabel, 0, 0);
 
-        //TextArea
-//        TextArea opmerkingenArea = new TextArea();
-//        opmerkingenArea.setId("opmerkingenTexfield");
-//        textAreaPane.add(opmerkingenArea, 0, 1);
-
+        //TextArea ListView
         ObservableList<String> opmerkingenText = FXCollections.observableArrayList();;
         Collections.sort(opmerkingenText);
         ListView<String> opmerkingenTextView = new ListView<String>(opmerkingenText);
-        opmerkingenTextView.setId("attitudeListView");
+        opmerkingenTextView.setId("attitudeListViewOpmerkingen");
         textAreaPane.add(opmerkingenTextView, 0, 1);
-
+        
+        //image uitroepingsteken
+//        Image uitroepingstekenImg = new Image("Images/uitroepingsteken.png.png", Math.ceil(schermformaat.getWidth()*0.03), USE_PREF_SIZE, true, true);
+//        ImageView uitroepingstekenView = new ImageView(uitroepingstekenImg);
+        
         //Knoppen
         Button voegToe = new Button("Voeg Toe");
         voegToe.setId("inlogButtons");
@@ -156,11 +166,9 @@ public class AttitudePaneel extends GridPane {
             if (invulTextField.getText().trim().isEmpty() == false) {
                 opmerkingenView.getSelectionModel().clearSelection();
                 opmerkingenText.add(invulTextField.getText() + "\n");
-//                opmerkingenText.appendText(invulTextField.getText() + "\n");
                 invulTextField.clear();
             } else if (opmerkingenView.getSelectionModel().getSelectedItem() != null) {
                 opmerkingenText.add(opmerkingenView.getSelectionModel().getSelectedItem() + "\n");
-//                opmerkingenText.appendText(opmerkingenView.getSelectionModel().getSelectedItem() + "\n");
                 opmerkingenView.getSelectionModel().clearSelection();
             }
         });
@@ -172,16 +180,27 @@ public class AttitudePaneel extends GridPane {
 
         verwijder.setOnMouseClicked(event -> {
             opmerkingenText.remove(opmerkingenTextView.getSelectionModel().getSelectedItem());
-//            opmerkingenArea.clear();
         });
 
         //
         Button terug = new Button("Terug");
         terug.setId("inlogButtons");
-        knopPane.add(terug, 1, 0);
+        add(terug, 2, 2);
 
         terug.setOnMouseClicked(event -> {
             scene.setRoot(hoofdPanel);
+        });
+        
+        //
+        Button aandacht = new Button("Aandacht");
+        aandacht.setId("inlogButtons");
+        knopPane.add(aandacht, 1, 0);
+
+        aandacht.setOnMouseClicked(event -> {
+//            Collections.replaceAll(opmerkingenText, opmerkingenTextView.getSelectionModel().getSelectedItem(), opmerkingenTextView.getSelectionModel().getSelectedItem() + "!");
+//            String item = opmerkingenTextView.getSelectionModel().getSelectedItem();
+//            opmerkingenText.remove(opmerkingenTextView.getSelectionModel().getSelectedItem());
+//            opmerkingenText.add(item + "!" + "\n");
         });
 
     }
